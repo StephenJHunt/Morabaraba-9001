f5d2a3b38c8b351679b8b6d2bbae2baa23d6dea7
g16h0403 <g16h0403@HUG158>
2018-04-12 14:34:20 +0200

    Create initial interfaces

--- Morabaraba-9001/Program.cs
+++ Morabaraba-9001/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Morabaraba_9001
 {
+    interface IBoard
+    {
+        ICow occupant(string pos);
+        IEnumerable<int> Cows(Player player);
+        void Move(string piecePos, string movePos);
+        void Shoot(string shootPos);
+        void makeFlying(Player player);
+    }
+    interface ICow
+    {
+        bool isMovable();
+        bool isInMill();
+        void Move(string movePos);
+        string getPosition();
+        CowStatus GetStatus();
+    }
+    interface IPlayer
+    {
+        Player player { get; }
+        string getMove(string prompt);
+
+    }
+    public enum Player { X, O }
+    public enum CowStatus {Unplaced, Placed, Flying, Shot}
+    interface IGameManager
+    {
+        void startGame();
+        void placingPhase();
+        void movingPhase();
+        void endGame();
+
+    }
+    public class invalidMoveException : ApplicationException { }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
