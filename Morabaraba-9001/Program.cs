1279e9c9007ffcbe49f388a2121a3726515e8baf
g16h0403 <g16h0403@HUG158>
2018-04-12 15:08:52 +0200

    Create board constructor

--- Morabaraba-9001/Program.cs
+++ Morabaraba-9001/Program.cs
@@ -38,6 +38,15 @@ namespace Morabaraba_9001
     public class invalidMoveException : ApplicationException { }
     public class Board : IBoard
     {
+        Dictionary<string, CowStatus> board = new Dictionary<string, CowStatus>();
+        public Board()
+        {
+            string[] positions = new string[] {"A1", "A4", "A7", "B2", "B4", "B6" , "C3", "C4", "C5", "D1", "D2", "D3", "D5", "D6", "D7", "E3", "E4", "E5", "F2", "F4", "F6", "G1", "G4", "G7" };
+            foreach (string pos in positions)//initialising board with empty values
+            {
+                board.Add(pos, CowStatus.Unplaced);
+            }
+        }
         public IEnumerable<int> Cows(Player player)
         {
             throw new NotImplementedException();
