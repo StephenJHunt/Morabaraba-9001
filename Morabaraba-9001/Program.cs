6e452c3bff4e6b92cab50f4e84756298603335e0
g16h0403 <g16h0403@HUG158>
2018-04-12 14:47:04 +0200

    Create class stubs

--- Morabaraba-9001/Program.cs
+++ Morabaraba-9001/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Morabaraba_9001
 {
-    interface IBoard
+    public interface IBoard
     {
         ICow occupant(string pos);
         IEnumerable<int> Cows(Player player);
@@ -11,7 +11,7 @@ namespace Morabaraba_9001
         void Shoot(string shootPos);
         void makeFlying(Player player);
     }
-    interface ICow
+    public interface ICow
     {
         bool isMovable();
         bool isInMill();
@@ -19,7 +19,7 @@ namespace Morabaraba_9001
         string getPosition();
         CowStatus GetStatus();
     }
-    interface IPlayer
+    public interface IPlayer
     {
         Player player { get; }
         string getMove(string prompt);
@@ -27,7 +27,7 @@ namespace Morabaraba_9001
     }
     public enum Player { X, O }
     public enum CowStatus {Unplaced, Placed, Flying, Shot}
-    interface IGameManager
+    public interface IGameManager
     {
         void startGame();
         void placingPhase();
@@ -36,6 +36,98 @@ namespace Morabaraba_9001
 
     }
     public class invalidMoveException : ApplicationException { }
+    public class Board : IBoard
+    {
+        public IEnumerable<int> Cows(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void makeFlying(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(string piecePos, string movePos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICow occupant(string pos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shoot(string shootPos)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class PowerLevel9000Cow : ICow
+    {
+        public string getPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CowStatus GetStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isInMill()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isMovable()
+        {
+            throw new NotImplementedException();
+        }
+
+        virtual public void Move(string movePos)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class FlyingPowerLevel9001Cow : PowerLevel9000Cow
+    {
+        public override void Move(string movePos)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class MorabarabaManager : IGameManager
+    {
+        public void endGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void movingPhase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void placingPhase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void startGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class GamePlayer : IPlayer
+    {
+        public Player player => throw new NotImplementedException();
+
+        public string getMove(string prompt)
+        {
+            throw new NotImplementedException();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
