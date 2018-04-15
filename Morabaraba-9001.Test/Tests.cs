567446eef2091b54d6bfa4ecb284492cfc2a4a5d
Dane Horn <g16H0473@campus.ru.ac.za>
2018-04-15 17:40:45 +0200

    Adjust testing that cows can only be placed on empty squares

--- Morabaraba-9001.Test/Tests.cs
+++ Morabaraba-9001.Test/Tests.cs
@@ -27,29 +27,20 @@ namespace Morabaraba_9001.Test
         [Test]
         public void PlayerXStartsFirst()//Player X is our equivalent for the dark cows player
         {
-            MorabarabaManager manager = Substitute.For<MorabarabaManager>();
+            MorabarabaManager manager = new MorabarabaManager();
             Assert.That(manager.currPlayer.playerID == Player.X);
         }
         [Test]
         public void CowsCanOnlyBePlayedOnEmptySpaces()
         {
-
-            //X places on A1
-            //O tries to place on A1, will need to re-enter a new (empty) position which will be A4
-            //If the test works A1 will be X and A4 will be O
-            //This shows that a player's piece will not be replaced and that a piece can be placed normally on an empty position
-            Board b = new Board();
+            Board b = Substitute.For<Board>();
+            b.board["A1"] = new Cell(Player.O);
+            b.board["A4"] = new Cell(Player.None);
             IPlayer x = Substitute.For<IPlayer>();
-            IPlayer o = Substitute.For<IPlayer>();
-            x.setID(Player.X);
-            o.setID(Player.O);
-
-            x.getMove(Arg.Any<string>()).Returns("A1");
-            o.getMove(Arg.Any<string>()).Returns("A1", "A4");
-
+            x.playerID.Returns(Player.X);
+            x.getMove(Arg.Any<string>()).Returns("A1", "A4");
             b.Place(x);
-            b.Place(o);
-            Assert.That(b.board["A1"].getState == x.playerID && b.board["A4"].getState == o.playerID);
+            Assert.That(b.board["A1"].getState == Player.O && b.board["A4"].getState == Player.X);
         }
         [Test]
         public void AMaximumOf12PlacementsPerPlayerAreAllowed()
